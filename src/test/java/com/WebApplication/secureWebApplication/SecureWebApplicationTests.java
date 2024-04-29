dcfb4df4197a336c2a045ab85ec9457d9a423038
Krishna Modi <krishnamodi41423@gmail.com>
2024-04-30 00:35:44 +0530

    Krishna :: Adding a secure spring web application

---
+++ src/test/java/com/WebApplication/secureWebApplication/SecureWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WebApplication.secureWebApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
